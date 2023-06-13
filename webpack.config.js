a1fb8564d10693f0d20548dd9bd3b8ae82fb1665
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-06-13 17:12:00 +0300

    aAdded linters

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name][contenthash].js",
+        clean: true,
+        assetModuleFileName: "[name][ext]"
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        hot: true,
+        open: true,
+        compress: true,
+        historyApiFallback: true
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpeg|jpeg|gif)$/i,
+                type: 'assets/source'
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "todo",
+            filename: "index.html",
+            template: "src/template.html"
+        })
+    ]
+}
